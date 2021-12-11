441967cc91366fe016dce60f010e8d6d1005a490
Oleksandra Kryvobok <88559507+OleksandraKryvobok@users.noreply.github.com>
2021-12-11 22:28:55 +0200

    Создает и рендерит разметку галереи, реализ.делегирование на контейнере

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createMarkupOfImgCard(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createMarkupOfImgCard(gallery) {
+    return gallery.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}" target="_self">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>
+        `
+    }).join('');
+}
+
+const onImageCardClick = (e) => {
+    const isImgEl = e.target.classList.contains('gallery__image');
+    if (!isImgEl) {
+        return;
+    }
+    
+}
+
+galleryContainer.addEventListener('click', onImageCardClick);
+
+
+
+
+
+
